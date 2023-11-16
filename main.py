f62e6c3ae0d575c47f0f8022bc36f3ec9d307e9f
Quentin <47141399+pepequentin@users.noreply.github.com>
2023-11-16 13:22:12 +0100

    Eng comment

--- main.py
+++ main.py
@@ -3,15 +3,11 @@ from tkinter import ttk
 import psutil
 import threading
 
-#pip install psutil
-#pip install customtkinter
+# Install required packages
+# pip install psutil
+# pip install customtkinter
 
-
-
-
-
-
-
+# Function to update the state of various processes
 def update_process_states(process_states):
     for process in psutil.process_iter(['pid', 'name']):
         if 'ankama launcher.exe' in process.info['name'].lower():
@@ -22,26 +18,25 @@ def update_process_states(process_states):
             process_states["excel.exe"] = True
     return process_states
 
-
-
+# Custom ToggleButton widget
 class ToggleButton(tk.Frame):
     def __init__(self, master=None, text="", initial_state=False, **kwargs):
         super().__init__(master, **kwargs)
         self.value = tk.BooleanVar(value=initial_state)
 
-        # Ajouter un texte à gauche du slider en utilisant un Label avec wraplength
+        # Add text to the left of the slider using a Label with wraplength
         self.left_label = tk.Label(self, text=text, anchor="w", padx=5)
         self.left_label.grid(row=0, column=0, sticky="w")
 
-        # Créer un slider
+        # Create a slider
         self.slider = ttk.Checkbutton(self, variable=self.value, command=self.toggle)
         self.slider.grid(row=0, column=1, padx=5, pady=5)
 
-        # Ajouter un texte à droite du slider en utilisant un Label avec wraplength
+        # Add text to the right of the slider using a Label with wraplength
         self.right_label = tk.Label(self, text="Off", anchor="w", padx=5, foreground="red")
         self.right_label.grid(row=0, column=2, sticky="w")
 
-        # Mettre à jour le texte et la couleur en fonction de l'état initial
+        # Update text and color based on the initial state
         self.update_label()
 
     def toggle(self):
@@ -57,21 +52,22 @@ class ToggleButton(tk.Frame):
         self.value.set(state)
         self.update_label()
 
+# Main application class
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Toggle Buttons")
 
-        # Dictionnaire pour stocker l'état de chaque processus
+        # Dictionary to store the state of each process
         process_states = {
             'Ankama Launcher.exe': False,
             'brave.exe': False,
             'excel.exe': False,
-            # Ajoutez d'autres processus si nécessaire
+            # Add other processes if needed
         }
         process_states = update_process_states(process_states)
 
-        # Créer plusieurs ToggleButtons avec des textes différents et des états initiaux
+        # Create multiple ToggleButtons with different texts and initial states
         self.toggle1 = ToggleButton(self, text="Ankama", initial_state=process_states["Ankama Launcher.exe"])
         self.toggle1.grid(row=0, column=0, padx=5, pady=5, sticky="w")
 
@@ -81,33 +77,31 @@ class App(tk.Tk):
         self.toggle3 = ToggleButton(self, text="Excel", initial_state=process_states["excel.exe"])
         self.toggle3.grid(row=2, column=0, padx=5, pady=5, sticky="w")
 
-        # Ajuster la taille de la fenêtre
+        # Adjust the window size
         self.geometry("500x200")
 
-        # Lancer la boucle de vérification des processus
-        # self.check_processes()
-        # Lancer la boucle de vérification des processus dans un thread
+        # Start the process checking loop in a thread
         self.thread = threading.Thread(target=self.check_processes)
-        self.thread.daemon = True  # Le thread s'arrêtera lorsque le programme principal se terminera
+        self.thread.daemon = True  # The thread will stop when the main program ends
         self.thread.start()
-        
+
     def check_processes(self):
         while True:
 
-            # Dictionnaire pour stocker l'état de chaque processus
+            # Dictionary to store the state of each process
             process_states = {
                 'Ankama Launcher.exe': False,
                 'brave.exe': False,
                 'excel.exe': False,
-                # Ajoutez d'autres processus si nécessaire
+                # Add other processes if needed
             }
             process_states = update_process_states(process_states)
-            # Vérifier l'état des processus
+            # Check the state of processes
             self.toggle1.set_initial_state(process_states['Ankama Launcher.exe'])
             self.toggle2.set_initial_state(process_states['brave.exe'])
             self.toggle3.set_initial_state(process_states['excel.exe'])
             
-            # Attendre 4 secondes avant la prochaine vérification
+            # Wait for 4 seconds before the next check
             threading.Event().wait(4)
 
 if __name__ == "__main__":
