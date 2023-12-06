7d3f075755893bc7d4e3ce8d2cb95ae5fbbc9d11
Quentin <47141399+pepequentin@users.noreply.github.com>
2023-12-06 10:03:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import ttk
 import psutil
 import threading
-
 # Install required packages
 # pip install psutil
 # pip install customtkinter
@@ -20,9 +19,11 @@ def update_process_states(process_states):
 
 # Custom ToggleButton widget
 class ToggleButton(tk.Frame):
-    def __init__(self, master=None, text="", initial_state=False, **kwargs):
+    def __init__(self, master=None, text="", initial_state=False, on_callback=None, off_callback=None, **kwargs):
         super().__init__(master, **kwargs)
         self.value = tk.BooleanVar(value=initial_state)
+        self.on_callback = on_callback
+        self.off_callback = off_callback
 
         # Add text to the left of the slider using a Label with wraplength
         self.left_label = tk.Label(self, text=text, anchor="w", padx=5)
@@ -41,6 +42,11 @@ class ToggleButton(tk.Frame):
 
     def toggle(self):
         self.update_label()
+        # Call the appropriate callback function based on the state
+        if self.value.get() and self.on_callback:
+            self.on_callback()
+        elif not self.value.get() and self.off_callback:
+            self.off_callback()
 
     def update_label(self):
         if self.value.get():
@@ -68,13 +74,17 @@ class App(tk.Tk):
         process_states = update_process_states(process_states)
 
         # Create multiple ToggleButtons with different texts and initial states
-        self.toggle1 = ToggleButton(self, text="Ankama", initial_state=process_states["Ankama Launcher.exe"])
+        # Pass the on_callback and off_callback functions to the ToggleButtons
+        self.toggle1 = ToggleButton(self, text="Ankama", initial_state=process_states["Ankama Launcher.exe"],
+                                    on_callback=self.on_toggle1_on, off_callback=self.on_toggle1_off)
         self.toggle1.grid(row=0, column=0, padx=5, pady=5, sticky="w")
 
-        self.toggle2 = ToggleButton(self, text="Brave", initial_state=process_states["brave.exe"])
+        self.toggle2 = ToggleButton(self, text="Brave", initial_state=process_states["brave.exe"],
+                                    on_callback=self.on_toggle2_on, off_callback=self.on_toggle2_off)
         self.toggle2.grid(row=1, column=0, padx=5, pady=5, sticky="w")
 
-        self.toggle3 = ToggleButton(self, text="Excel", initial_state=process_states["excel.exe"])
+        self.toggle3 = ToggleButton(self, text="Excel", initial_state=process_states["excel.exe"],
+                                    on_callback=self.on_toggle3_on, off_callback=self.on_toggle3_off)
         self.toggle3.grid(row=2, column=0, padx=5, pady=5, sticky="w")
 
         # Adjust the window size
@@ -87,7 +97,6 @@ class App(tk.Tk):
 
     def check_processes(self):
         while True:
-
             # Dictionary to store the state of each process
             process_states = {
                 'Ankama Launcher.exe': False,
@@ -100,9 +109,30 @@ class App(tk.Tk):
             self.toggle1.set_initial_state(process_states['Ankama Launcher.exe'])
             self.toggle2.set_initial_state(process_states['brave.exe'])
             self.toggle3.set_initial_state(process_states['excel.exe'])
-            
+
             # Wait for 4 seconds before the next check
             threading.Event().wait(4)
+
+    # Callback functions for ToggleButton 1
+    def on_toggle1_on(self):
+        print("Toggle 1 turned ON")
+
+    def on_toggle1_off(self):
+        print("Toggle 1 turned OFF")
+
+    # Callback functions for ToggleButton 2
+    def on_toggle2_on(self):
+        print("Toggle 2 turned ON")
+
+    def on_toggle2_off(self):
+        print("Toggle 2 turned OFF")
+
+    # Callback functions for ToggleButton 3
+    def on_toggle3_on(self):
+        print("Toggle 3 turned ON")
+
+    def on_toggle3_off(self):
+        print("Toggle 3 turned OFF")
 
 if __name__ == "__main__":
     app = App()
